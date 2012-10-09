6dfb72a6d0d1d7f319ef6a228a72347acbee8cdd
Matthew Smith <smith_matthew@live.com>
2012-10-09 09:18:34 -0400

    added code to branch

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,10 +8,18 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.print("Hello World");
+		System.out.println("Hello World");
 	}
 	
 	public String hello() {
 		return "goodbye";
+	}
+	
+	/**
+	 * I forgot to switch branches when I did this the first time
+	 * @return
+	 */
+	public String experimentalCode() {
+		return "Experiement";
 	}
 }
